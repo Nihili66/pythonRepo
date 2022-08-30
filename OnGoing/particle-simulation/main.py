00ed65c117aa1f0186b27d4e081be2ec5a25e189
Nihili66 <hichaza1@gmail.com>
2022-08-30 16:52:38 +0100

    New particle simulation project

---
+++ OnGoing/particle-simulation/main.py
@@ -0,0 +1,30 @@
+import pygame
+import sys
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(TITLE)
+        # screen settings
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.background = pygame.Surface((WIDTH, HEIGHT))
+        self.background.fill(pygame.Color('#000000'))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                # quit game
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.screen.blit(self.background, (0, 0))
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
